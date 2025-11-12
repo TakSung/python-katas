775b1c6a5ceabbb560e536ac2580c4bb830e390f
taktakgyun <vbmrkk@naver.com>
2025-11-12 21:37:56 +0900

    ⚡️ feat : game guess 구현

--- hidden-number/domain/game.py
+++ hidden-number/domain/game.py
@@ -1,4 +1,26 @@
+# -*- coding: utf-8 -*-
 # Game Domain Entity
 # TODO: dataclass를 사용하여 Game 엔티티를 정의합니다
 # - 불변성(frozen=True)
 # - 게임 상태 (숨겨진 숫자, 시도 횟수, 게임 종료 여부 등)
+
+from dataclasses import dataclass
+from enum import Enum
+
+class AnswerType(Enum):
+    TOO_LOW = "더 낮음"
+    TOO_HIGH = "더 높음"
+    CORRECT = "정답"
+@dataclass
+class Game:
+    secret_number:int
+    
+    def guess(self, guess_number:int) -> AnswerType:
+        if self.secret_number > guess_number:
+            return AnswerType.TOO_LOW
+        elif self.secret_number == guess_number:
+            return AnswerType.CORRECT
+        else:
+            return AnswerType.TOO_HIGH
+
+    
--- hidden-number/tests/test_game.py
+++ hidden-number/tests/test_game.py
@@ -1,2 +1,34 @@
 # Test for Game Domain Entity
 # TODO: Game 엔티티에 대한 단위 테스트를 작성합니다
+
+from domain.game import Game, AnswerType
+
+def test_추측한_숫자가_정답보다_작은_경우():
+    # given
+    game = Game(secret_number=50)
+    
+    # when
+    result = game.guess(30)
+    
+    # then
+    assert result == AnswerType.TOO_LOW
+    
+def test_추측한_숫자가_정답보다_큰_경우():
+    # given
+    game = Game(secret_number=50)
+    
+    # when
+    result = game.guess(70)
+    
+    # then
+    assert result == AnswerType.TOO_HIGH
+    
+def test_추측한_숫자가_정답과_같은_경우():
+    # given
+    game = Game(secret_number=50)
+    
+    # when
+    result = game.guess(50)
+    
+    # then
+    assert result == AnswerType.CORRECT
