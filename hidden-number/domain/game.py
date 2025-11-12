adaba038a41a8b6360f4c4f94271da700d74b021
joshuaj21 <joshuajung360@gmail.com>
2025-11-12 21:37:55 +0900

    Hidden Number 구현

--- hidden-number/domain/game.py
+++ hidden-number/domain/game.py
@@ -2,3 +2,22 @@
 # TODO: dataclass를 사용하여 Game 엔티티를 정의합니다
 # - 불변성(frozen=True)
 # - 게임 상태 (숨겨진 숫자, 시도 횟수, 게임 종료 여부 등)
+
+from enum import Enum, auto
+
+class GuessResult(Enum):
+    TOO_LOW = auto()
+    TOO_HIGH = auto()
+    CORRECT = auto()
+
+class Game:
+    def __init__(self, secret_number):
+        self.secret_number = secret_number
+
+    def guess(self, guessed_number):
+        if guessed_number < self.secret_number:
+            return GuessResult.TOO_LOW
+        elif guessed_number > self.secret_number:
+            return GuessResult.TOO_HIGH
+        else:
+            return GuessResult.CORRECT 
--- hidden-number/tests/test_game.py
+++ hidden-number/tests/test_game.py
@@ -1,2 +1,22 @@
 # Test for Game Domain Entity
 # TODO: Game 엔티티에 대한 단위 테스트를 작성합니다
+
+from domain.game import Game, GuessResult
+ 
+
+     
+def test_축측한숫자가정답보다작은경우():
+    game = Game(secret_number=50)
+    result = game.guess(30)
+    assert result == GuessResult.TOO_LOW
+    
+def test_축측한숫자가정답인경우():
+    game = Game(secret_number=50)
+    result = game.guess(50)
+    assert result == GuessResult.CORRECT
+    
+def test_축측한숫자가정답보다큰경우():
+    game = Game(secret_number=50)
+    result = game.guess(80)
+    assert result == GuessResult.TOO_HIGH
+
