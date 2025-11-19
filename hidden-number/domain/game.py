ca8e897d565843f07f72d4c2c176d4acb7fc9a54
joshuaj21 <joshuajung360@gmail.com>
2025-11-19 21:06:04 +0900

    경계값 확인

--- hidden-number/domain/game.py
+++ hidden-number/domain/game.py
@@ -9,15 +9,21 @@ class GuessResult(Enum):
     TOO_LOW = auto()
     TOO_HIGH = auto()
     CORRECT = auto()
+    OUT_OF_RANGE = auto()
 
 class Game:
-    def __init__(self, secret_number):
+    def __init__(self, secret_number: int, attempts: int = 0):
         self.secret_number = secret_number
+        self.attempts = attempts
 
     def guess(self, guessed_number):
+        self.attempts += 1
         if guessed_number < self.secret_number:
             return GuessResult.TOO_LOW
         elif guessed_number > self.secret_number:
             return GuessResult.TOO_HIGH
+        elif guessed_number < 1 or guessed_number > 100:
+            return GuessResult.OUT_OF_RANGE
         else:
             return GuessResult.CORRECT 
+            
