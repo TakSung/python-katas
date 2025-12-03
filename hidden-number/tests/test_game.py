3391b62fb8dca3a7fb2caaf7565045ee445a833b
taktakgyun <vbmrkk@naver.com>
2025-12-03 14:18:28 +0900

    🧪test : 게임 시도횟수 테스트 추가

--- hidden-number/tests/test_game.py
+++ hidden-number/tests/test_game.py
@@ -32,3 +32,33 @@ def test_추측한_숫자가_정답과_같은_경우():
     
     # then
     assert result == AnswerType.CORRECT
+    
+def test_횟수_추측():
+    # given
+    game = Game(secret_number=50)
+    
+    # when
+    last_num = 0
+    for i in range(1,100):
+        last_num = i
+        result = game.guess(i)
+        if result == AnswerType.CORRECT :
+            break
+    
+    # then
+    assert last_num == 50
+    assert game.get_attempt() == 50
+
+def test_최대_시도_횟수_검증():
+    # given
+    game = Game(secret_number=50)
+    max_attempt = 10 
+    assert max_attempt == game.get_max_attempt()
+    
+    # when
+    for i in range(max_attempt+1):
+        assert game.is_game_over() == False
+        game.guess(49)
+    
+    # then
+    assert game.is_game_over() == True
