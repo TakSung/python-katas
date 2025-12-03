ac87d207df722569a6b1b6d3b3a9d195473c5029
joshuaj21 <joshuajung360@gmail.com>
2025-12-03 21:07:07 +0900

    ui 기능 구현 완료

--- hidden-number/ui/tkinter_ui.py
+++ hidden-number/ui/tkinter_ui.py
@@ -6,12 +6,14 @@
 # - 새 게임 버튼
 import tkinter as tk 
 from app.game_service import GameService
+from domain.game import GuessResult
 
 
 class TkinterUI:
     def __init__(self,root, game_service: GameService):
         self.root = root
-        self.game_service = game_service    
+        self.game_service = game_service
+        self.game_service.new_game()    
         self.root.title("Hidden Number 게임")
         self.root.geometry("300x200")
 
@@ -27,7 +29,33 @@ class TkinterUI:
         self.result_label.pack(pady = 10)
         
     def handle_guess(self):
-        pass
+        try:
+            guessed_number_str = self.entry_var.get()
+            guessed_number = int(guessed_number_str)
+            
+            result_enum = self.game_service.process_guess(guessed_number)  
+            
+            message = ""
+            match result_enum:
+                case GuessResult.TOO_HIGH:
+                    message = "더 작은 수입니다!"
+                case GuessResult.TOO_LOW:
+                    message = "더 큰 수입니다!"
+                case GuessResult.CORRECT:
+                    message = "정답입니다!"
+                case _:
+                    message = "알 수 없는 결과입니다."
+                    
+            self.result_label.config(text= message)
+        
+        except ValueError:
+            self.result_label.config(text= "유요한 숫자를 입력해주세요.")
+            
+        except Exception as e:
+            self.result_label.config(text=f"오류 발생:{e}")
+    
+        
+    
 
     def start(self):
         self.root.mainloop()
