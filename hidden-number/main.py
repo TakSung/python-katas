aaa4f995852640b7c7a354a23903bb7eda35126b
taktakgyun <vbmrkk@naver.com>
2025-12-04 12:35:34 +0900

    💄ui : 뼈대 생성

--- hidden-number/main.py
+++ hidden-number/main.py
@@ -1,2 +1,39 @@
 # Hidden Number Game - Entry Point
 # TODO: 여기서 의존성을 조립하고 게임을 시작합니다
+
+from tkinter import Tk
+from dataclasses import dataclass
+
+from app.game_service import GameService
+from infra.random_generator import RandomNumberGenerator, NumberGenerator
+from ui.tkinter_ui import TkinterUI
+
+@dataclass
+class ObjectContainer:
+    generator : NumberGenerator
+    game_service : GameService
+    ui : TkinterUI
+    tk : Tk
+
+def inject_object_dependency()-> ObjectContainer:
+    generator = RandomNumberGenerator()
+    game_service = GameService(generator)
+    game_service.new_game()
+    tk = Tk()
+    ui = TkinterUI(root=tk, game_service=game_service)
+    
+    return ObjectContainer(
+        generator=generator,
+        game_service=game_service,
+        ui = ui,
+        tk = tk
+    )
+
+def run(container : ObjectContainer):
+    ui = container.ui
+    
+    ui.start_app()
+
+if __name__ == "__main__":
+    container = inject_object_dependency()
+    run(container)
--- hidden-number/ui/tkinter_ui.py
+++ hidden-number/ui/tkinter_ui.py
@@ -4,3 +4,33 @@
 # - 추측 버튼
 # - 결과 표시 레이블
 # - 새 게임 버튼
+
+import tkinter as tk
+from tkinter import Tk, Label, Entry, Button
+
+from app.game_service import GameService
+
+
+class TkinterUI:
+    
+    def __init__(self, root:Tk, game_service:GameService):
+        root.title("Hidden Number Game")
+        root.geometry("300x200+500+300")
+        label = Label(root, text="1부터 100 사이의 숫자를 맞춰보세요", font=("나눔고딕",16), bg="lightgray")
+        entry_field = Entry(root, width=10, font=("나눔고딕",20))
+        guess_button = Button(root, text="추측", font=("나눔고딕",16))
+        
+        
+        label.pack(pady=30, side=tk.TOP)
+        
+        guess_button.pack(pady=5, side=tk.BOTTOM)
+        entry_field.pack(pady=5, side=tk.BOTTOM)
+        
+        self.root = root
+        self.entry_field = entry_field
+        self.guess_button = guess_button
+        self.message_label = label
+        self.game_service = game_service
+        
+    def start_app(self):
+        self.root.mainloop()
